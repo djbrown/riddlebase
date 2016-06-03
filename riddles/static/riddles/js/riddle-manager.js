911a2da4028e65ab53a4b5e1f7e3f82b9b17b338
dan-brown <daniel.brown@live.de>
2016-06-04 01:53:33 +0200

    refactored code to fix metrics issues

--- riddles/static/riddles/js/riddle-manager.js
+++ riddles/static/riddles/js/riddle-manager.js
@@ -1,4 +1,4 @@
-RiddleManager = (function () {
+var RiddleManager = (function () {
 
     const RIDDLE_CELL_SIZE = 30;
     const PICKER_CELL_SIZE = 25;
@@ -6,22 +6,58 @@ RiddleManager = (function () {
     const DEFAULT_ZOOM_FACTOR = 1.4;
     var zoomFactor = 1;
 
-    function init() {
-        var zoomDelta = 0.2;
+    function applyZoom() {
+        document.getElementById("full-screen").classList.remove("active");
+        var svg = document.getElementById("riddle");
+        svg.classList.remove("full-screen");
+        //noinspection JSUnresolvedFunction
+        var bBox = svg.getBBox();
 
-        $("#shrink").click(createSetZoomDeltaFunction(-zoomDelta));
-        $("#restore-size").click(resetZoom);
-        $("#enlarge").click(createSetZoomDeltaFunction(zoomDelta));
-        $("#full-screen").click(toggleFullScreen);
+        var width = bBox.width + 2 * OFFSET;
+        var svgWidth = width * zoomFactor;
 
-        if (document.getElementById("save") !== null) {
-            initUserControls();
+        svg.setAttribute("width", svgWidth.toString());
+    }
+
+    function setZoomFactor(newZoomFactor) {
+        if (newZoomFactor < 0) {
+            return;
         }
+        zoomFactor = newZoomFactor;
+        applyZoom();
+    }
+
+    function resetZoom() {
+        setZoomFactor(DEFAULT_ZOOM_FACTOR);
+    }
+
+    function createSetZoomDeltaFunction(zoomDelta) {
+        return function () {
+            setZoomFactor(zoomFactor += zoomDelta);
+        };
+    }
+
+    function toggleFullScreen() {
+        var svg = document.getElementById("riddle");
+        svg.classList.toggle("full-screen");
+        document.getElementById("full-screen").classList.toggle("active");
     }
 
     function initUserControls() {
         // TODO: init user controls (save, restore, comment, rate)
-        console.log("initializing user controls")
+    }
+
+    function init() {
+        var zoomDelta = 0.2;
+
+        document.getElementById("shrink").addEventListener("click", createSetZoomDeltaFunction(-zoomDelta));
+        document.getElementById("restore-size").addEventListener("click", resetZoom);
+        document.getElementById("enlarge").addEventListener("click", createSetZoomDeltaFunction(zoomDelta));
+        document.getElementById("full-screen").addEventListener("click", toggleFullScreen);
+
+        if (document.getElementById("save") !== null) {
+            initUserControls();
+        }
     }
 
     function adjustViewBox() {
@@ -38,43 +74,6 @@ RiddleManager = (function () {
         svg.setAttribute("viewBox", viewBox);
     }
 
-    function applyZoom() {
-        document.getElementById("full-screen").classList.remove("active");
-        var svg = document.getElementById("riddle");
-        svg.classList.remove("full-screen");
-        //noinspection JSUnresolvedFunction
-        var bBox = svg.getBBox();
-
-        var width = bBox.width + 2 * OFFSET;
-        var svgWidth = width * zoomFactor;
-
-        svg.setAttribute("width", svgWidth.toString());
-    }
-
-    function createSetZoomDeltaFunction(zoomDelta) {
-        return function () {
-            setZoomFactor(zoomFactor += zoomDelta);
-        };
-    }
-
-    function resetZoom() {
-        setZoomFactor(DEFAULT_ZOOM_FACTOR);
-    }
-
-    function toggleFullScreen() {
-        var svg = document.getElementById("riddle");
-        svg.classList.toggle("full-screen");
-        document.getElementById("full-screen").classList.toggle("active");
-    }
-
-    function setZoomFactor(newZoomFactor) {
-        if (newZoomFactor < 0) {
-            return;
-        }
-        zoomFactor = newZoomFactor;
-        applyZoom();
-    }
-
     function createSvgElement(qualifiedName) {
         return document.createElementNS("http://www.w3.org/2000/svg", qualifiedName);
     }
@@ -82,7 +81,7 @@ RiddleManager = (function () {
     function check(proposal, correctFallbackFunction) {
         var ajax = new XMLHttpRequest();
         ajax.onreadystatechange = function () {
-            if (ajax.readyState == 4 && ajax.status == 200) {
+            if (ajax.readyState === 4 && ajax.status === 200) {
                 var response = JSON.parse(ajax.responseText);
                 if (response.correct === true) {
                     correctFallbackFunction();
@@ -95,12 +94,12 @@ RiddleManager = (function () {
     }
 
     return {
-        RIDDLE_CELL_SIZE: RIDDLE_CELL_SIZE,
-        PICKER_CELL_SIZE: PICKER_CELL_SIZE,
-        init: init,
-        resetZoom: resetZoom,
-        adjustViewBox: adjustViewBox,
-        createSvgElement: createSvgElement,
-        check: check
+        RIDDLE_CELL_SIZE,
+        PICKER_CELL_SIZE,
+        init,
+        resetZoom,
+        adjustViewBox,
+        createSvgElement,
+        check
     };
-})();
+}());
