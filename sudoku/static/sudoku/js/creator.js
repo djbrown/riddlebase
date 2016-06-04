ab7ab80a32e615b84c43e8c0d73fd8e31fe35bfa
dan-brown <daniel.brown@live.de>
2016-06-04 02:10:52 +0200

    refactored code to fix metrics issues

--- sudoku/static/sudoku/js/creator.js
+++ sudoku/static/sudoku/js/creator.js
@@ -1,5 +1,25 @@
-Creator = function () {
+var Creator = (function () {
     var csrfToken;
+
+    function handleSubmitResponse(json) {
+
+    }
+
+    function submit() {
+        var ajax = new XMLHttpRequest();
+        ajax.onreadystatechange = function () {
+            if (ajax.readyState === 4 && ajax.status === 200) {
+                handleSubmitResponse(JSON.parse(ajax.responseText))
+            }
+        };
+        ajax.open("POST", "../submit-new/", true);
+        ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        ajax.setRequestHeader("X-CSRFToken", csrfToken);
+
+        var data = "solution=123";
+        data += "&pattern=123";
+        ajax.send(data);
+    }
 
     function init(_csrfToken) {
         csrfToken = _csrfToken;
@@ -8,27 +28,7 @@ Creator = function () {
         submitButton.addEventListener("click", submit);
     }
 
-    function handleSubmitResponse(data) {
-        console.log(data);
-    }
-
-    function submit() {
-        var ajax = new XMLHttpRequest();
-        ajax.onreadystatechange = function () {
-            if (ajax.readyState == 4 && ajax.status == 200) {
-                var response = JSON.parse(ajax.responseText);
-                console.log(response);
-            }
-        };
-        ajax.open("POST", "../submit-new/", true);
-        ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        ajax.setRequestHeader("X-CSRFToken", csrfToken);
-        var data = "solution=123";
-        data += "&pattern=123";
-        ajax.send(data);
-    }
-
     return {
-        init: init
+        init
     }
-}();
+}());
